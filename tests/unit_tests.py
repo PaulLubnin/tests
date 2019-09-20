bccfc9cbf886ef4eb48820f75a64e0d62ebe5248
PaulLubnin <egopaul@inbox.ru>
2019-09-20 15:33:46 +0500

    добавил тест функций на удаление и добавление

--- tests/unit_tests.py
+++ tests/unit_tests.py
@@ -2,19 +2,19 @@ import unittest
 import json
 import os
 from ADPY4.tests.app import check_document_existance, get_all_doc_owners_names, \
-    append_doc_to_shelf
+    append_doc_to_shelf, remove_doc_from_shelf, documents, directories
 
 
-def json_load(file_path):
-    with open(file_path, 'r', encoding='utf-8') as file:
-        return json.load(file)
+# def json_load(file_path):
+#     with open(file_path, 'r', encoding='utf-8') as file:
+#         return json.load(file)
 
 class TestSecretaryAssistant(unittest.TestCase):
-    def setUp(self) -> None:
-        directories_path = os.path.join('fixtures', 'directories.json')
-        documents_path = os.path.join('fixtures', 'documents.json')
-        self.directories = json_load(directories_path)
-        self.documents = json_load(documents_path)
+    # def setUp(self) -> None:
+    #     directories_path = os.path.join('fixtures', 'directories.json')
+    #     documents_path = os.path.join('fixtures', 'documents.json')
+    #     self.directories = json_load(directories_path)
+    #     self.documents = json_load(documents_path)
 
     # def test_documents(self):
     #     self.assertTrue((type(self.documents) == list), 'Check loaded documents data')
@@ -27,12 +27,23 @@ class TestSecretaryAssistant(unittest.TestCase):
 
     def test_get_all_doc_owners_names(self):
         self.assertIsInstance(get_all_doc_owners_names(), set)
-        self.assertEqual(len(get_all_doc_owners_names()), len(self.documents))
+        self.assertEqual(len(get_all_doc_owners_names()), len(documents))
 
     def test_append_doc_to_shelf(self):
-        doc_number = '3'
         shelf_number = '1'
-        self.assertNotEqual(append_doc_to_shelf(doc_number, shelf_number), self.directories['1'])
+        before_append = len(directories[shelf_number])
+        new_doc_number = '3'
+        append_doc_to_shelf(new_doc_number, shelf_number)
+        after_append = len(directories[shelf_number])
+        self.assertGreater(after_append, before_append)
+
+    def test_remove_doc_from_shelf(self):
+        shelf_number = '1'
+        before_del = len(directories[shelf_number])
+        remove_doc = '11-2'
+        remove_doc_from_shelf(remove_doc)
+        after_del = len(directories[shelf_number])
+        self.assertGreater(before_del, after_del)
 
 
 if __name__ == '__main__':
